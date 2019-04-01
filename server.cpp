e4023222a6a1b7487ba30e2d949c78618d859949
Unknown <Josrodjr@gmail.com>
2019-04-01 17:49:27 -0600

    json implementation of a userlist for server

--- server.cpp
+++ server.cpp
@@ -7,10 +7,38 @@
 #include <string.h> 
 // Probar para JSON
 #include "json.hpp"
+#include <iostream>
 // Definitions
 #define PORT 8080
+// Que implemetacion de json estamos usando
+using json = nlohmann::json;
+
+using namespace std;
 
 int main(int argc, char const * argv[]){
+
+    // crear un dummy json solo para pruebas
+    json user;
+
+    user["id"] = 999;
+    user["username"] = "Pepega";
+    user["status"] = 2;
+    user["last_connected"] = "TIME_HERE";
+
+    std::cout << user["username"] << endl;
+
+    // create an array of users
+    json registered_users = json::array();
+    // append the new user
+    registered_users.push_back(user);
+    // check the array
+    cout << registered_users.size() << endl;
+    // print everything in the array of users
+    for (int i = 0; i < registered_users.size(); i++) 
+    {
+          std::cout << registered_users[i] << endl;
+    }
+
     int server_file_descriptor, new_socket, value; 
     struct sockaddr_in address; 
     int address_length = sizeof(address); 
